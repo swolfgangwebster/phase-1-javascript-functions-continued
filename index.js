ab57d4d4ee5402a173440e55e65c850fc08f5725
Shawn Webster <swolfgangwebster@gmail.com>
2023-03-15 23:28:01 -0500

    {}

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(a='roller-skate') {
+    return `This Saturday, I want to ${a}!`;
+}
+
+function mondayWork(a='go to the office') {
+    return `This Monday, I will ${a}.`;
+}
+
+function wrapAdjective(f) {
+    return function (a) {
+        return `You are ${f}${a}${f}!`
+    }
+}
